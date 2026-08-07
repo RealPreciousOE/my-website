5ef3ba9884f5d7b496f7d32ae1ec2432d49ab8b9
Precious <realoeprecious@gmail.com>
2026-08-07 14:35:52 -0700

    Added more JS function, worked on the darkmode button and menu button

--- script.js
+++ script.js
@@ -1,9 +1,70 @@
 
+ // Scrooll Behaviour
+   const sr = ScrollReveal ({
+    origin: "bottom",
+    distance: "60px",
+    duration: 900,
+    delay: 200,
+    reset: false
+  })
+
+  sr.reveal(".heroBox")
+  sr.reveal("#img", {
+   delay: 300
+  })
+
+  sr.reveal(".aboutColumn")
+
+  sr.reveal(".skillsColumn")
+
+  sr.reveal(".eduColumn")
+
+  sr.reveal(".certColumn", {
+   delay: 300
+  })
+
+  sr.reveal(".ProjectBox", {
+   interval: 200
+  })
+
+  sr.reveal(".contact")
+
+  sr.reveal("footer")
+
+
+
+
+
+
+
+
  const navItems = document.getElementsByClassName("navItems")[0]
  const menu = document.getElementsByClassName("menu")[0]
 
+ const navLinks = document.querySelectorAll(".navItems a")
+
  menu.addEventListener("click", () => {
     navItems.classList.toggle("active")
+
+    if(navItems.classList.contains("active")){
+      menu.textContent = "✕"
+    } else {
+      menu.textContent = "☰"
+    }
  })
+
+ navLinks.forEach(link => {
+   link.addEventListener("click", () => {
+     navItems.classList.remove("active")
+   })
+ })
+
+//  Dark mode funtion
+
+const darkMode = document.getElementsByClassName ("dark_mode")[0]
+
+darkMode.addEventListener("click", () => {
+   document.body.classList.toggle("active")
+})
 
  
