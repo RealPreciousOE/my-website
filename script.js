e313fe9c9c3965baa28c007e2e1d0b111e6dda8a
Precious <realoeprecious@gmail.com>
2026-08-08 14:30:14 -0700

    Completed my JavaScript functions

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+//  Dark mode funtion
+
+const darkMode = document.getElementsByClassName("dark_mode")[0];
+
+darkMode.addEventListener("click", () => {
+  document.body.classList.toggle("active");
+
+  if (document.body.classList.contains("active")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+const userTheme = localStorage.getItem("theme");
+if (userTheme === "dark") {
+  document.body.classList.add("active");
+} else {
+  document.body.classList.remove("active");
+}
+
 // Scrooll Behaviour
 const sr = ScrollReveal({
   origin: "bottom",
   distance: "60px",
   duration: 900,
-  delay: 200,
+  interval: 150,
   reset: false,
 });
 
@@ -13,7 +34,7 @@ sr.reveal("#img", {
   origin: "bottom",
   distance: "80px",
   duration: 1000,
-  interval: 200,
+  interval: 150,
   reset: false,
 });
 
@@ -21,31 +42,23 @@ sr.reveal(".text", {
   origin: "left",
   distance: "80px",
   duration: 1000,
-  interval: 200,
+  interval: 150,
   reset: false,
 });
 
-sr.reveal(".project_contact")
+sr.reveal(".project_contact");
 
-sr.reveal("#percorsoTech")
+sr.reveal("#percorsoTech");
 
 sr.reveal(".aboutColumn");
 
 sr.reveal(".skillsColumn");
 
-sr.reveal(".eduColumn", {
-   origin: "left",
-  distance: "80px",
-  duration: 1000,
-  interval: 200,
-  reset: false,
-});
+sr.reveal(".blue");
 
-sr.reveal
+sr.reveal(".eduColumn");
 
-sr.reveal(".certColumn", {
-  delay: 300,
-});
+sr.reveal(".certificate");
 
 sr.reveal(".techSkills", {
   origin: "left",
@@ -59,7 +72,7 @@ sr.reveal(".projectBox", {
   origin: "bottom",
   distance: "80px",
   duration: 1000,
-  interval: 200,
+  interval: 150,
   reset: false,
 });
 
@@ -67,7 +80,7 @@ sr.reveal(".projectDescription", {
   origin: "left",
   distance: "80px",
   duration: 1000,
-  interval: 200,
+  interval: 150,
   reset: false,
 });
 
@@ -75,7 +88,17 @@ sr.reveal(".description", {
   origin: "left",
   distance: "80px",
   duration: 1000,
-  interval: 200,
+  interval: 150,
+  reset: false,
+});
+
+sr.reveal(".certDescription");
+
+sr.reveal(".greet", {
+  origin: "left",
+  distance: "80px",
+  duration: 1000,
+  interval: 150,
   reset: false,
 });
 
@@ -83,35 +106,39 @@ sr.reveal(".intro", {
   origin: "left",
   distance: "80px",
   duration: 1000,
-  interval: 200,
+  interval: 150,
   reset: false,
 });
-
-
 
 sr.reveal(".content", {
   origin: "left",
   distance: "80px",
   duration: 1000,
-  interval: 200,
+  interval: 150,
   reset: false,
 });
 
 sr.reveal(".contact");
 
+sr.reveal(".certBox", {
+  origin: "left",
+  distance: "80px",
+  duration: 1000,
+  interval: 150,
+  reset: false,
+});
+
 sr.reveal(".mySkills", {
   origin: "left",
   distance: "80px",
   duration: 1000,
-  interval: 200,
+  interval: 150,
   reset: false,
 });
 
-
 const navItems = document.getElementsByClassName("navItems")[0];
-const menu = document.getElementsByClassName("menu")[0];
 
-const navLinks = document.querySelectorAll(".navItems a");
+const menu = document.getElementsByClassName("menu")[0];
 
 menu.addEventListener("click", () => {
   navItems.classList.toggle("active");
@@ -123,21 +150,27 @@ menu.addEventListener("click", () => {
   }
 });
 
+// Close the nav Bar when user taps on any link
+const navLinks = document.querySelectorAll(".navItems a");
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     navItems.classList.remove("active");
   });
 });
 
-//  Dark mode funtion
-
-const darkMode = document.getElementsByClassName("dark_mode")[0];
-
-darkMode.addEventListener("click", () => {
-  document.body.classList.toggle("active");
-});
-
 const greet = document.getElementsByClassName("greet")[0];
 greet.addEventListener("click", () => {
   greet.textContent = "Hi, I,m 👋";
+});
+
+// Function to close the navBar when user taps outside of it.
+
+document.addEventListener("click", (event) => {
+  if (
+    navItems.classList.contains("active") &&
+    !navItems.contains(event.target) &&
+    !menu.contains(event.target)
+  ) {
+    navItems.classList.remove("active");
+  }
 });
